37308b9679186d0cbd18b7cc49cf843ac5e2b874
luckysoftgo <supingemail@126.com>
2019-06-20 14:30:22 +0800

    redis 分布式锁实现.

--- base-cache/src/main/java/com/application/base/cache/redis/jedis/lock/DistributedLock.java
+++ base-cache/src/main/java/com/application/base/cache/redis/jedis/lock/DistributedLock.java
@@ -17,13 +17,21 @@ public interface DistributedLock {
      */
     String LOCK_SUCCESS = "OK";
     /**
-     * 这个参数我们填的是NX，意思是SET IF NOT EXIST,即当key不存在时,我们进行set操作;若key已经存在,则不做任何操作.
+     * 这个参数我们填的是NX，意思是SET IF NOT EXIST,即当key不存在时,我们进行set操作;否则,则不做任何操作.
      */
     String SET_IF_NOT_EXIST = "NX";
     /**
-     * 这个参数我们传的是PX，意思是我们要给这个key加一个过期的设置，具体时间由第五个参数决定
+     * 这个参数我们填的是XX，意思是SET IF ALREADY EXIST,即当key存在时,我们进行set操作;否则,则不做任何操作.
      */
-    String SET_WITH_EXPIRE_TIME = "PX";
+    String SET_IF_ALREADY_EXIST = "XX";
+    /**
+     * 这个参数我们传的是EX，意思是我们要给这个key加一个过期的设置，时间单位是秒
+     */
+    String SET_WITH_EXPIRE_SECONDS = "EX";
+    /**
+     * 这个参数我们传的是PX，意思是我们要给这个key加一个过期的设置，时间单位是毫秒
+     */
+    String SET_WITH_EXPIRE_MILLISECONDS = "PX";
     
     /**
      * 默认超时时间
@@ -39,13 +47,21 @@ public interface DistributedLock {
      * 根据uniqueKey构建分布式锁,对key进行锁定,循环获取锁,直到获取到锁为止
      * @param uniqueKey:唯一的key,
      * @param uniqueValue:请求的value,可以使用UUID来获取.
+     * @return
+     * @throws DistributedLockException
+     * @throws RedisException
+     */
+    boolean getDistLock(String uniqueKey,String uniqueValue) throws DistributedLockException,RedisException;
+    /**
+     * 根据uniqueKey构建分布式锁,对key进行锁定,循环获取锁,直到获取到锁为止
+     * @param uniqueKey:唯一的key,
+     * @param uniqueValue:请求的value,可以使用UUID来获取.
      * @param expireTime:毫秒数.
      * @return
      * @throws DistributedLockException
      * @throws RedisException
      */
     boolean getDistLock(String uniqueKey,String uniqueValue,int expireTime) throws DistributedLockException,RedisException;
-    
     /**
      * 释放 uniqueKey 对应的锁资源.
      * @param uniqueKey
@@ -55,42 +71,6 @@ public interface DistributedLock {
      * @throws RedisException
      */
     boolean releaseDistLock(String uniqueKey,String uniqueValue) throws DistributedLockException,RedisException;
-    
-    /**
-     * 根据uniqueKey构建分布式锁,对key进行锁定,循环获取锁,直到获取到锁为止
-     * @param uniqueKey
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean loopLock(String uniqueKey) throws DistributedLockException,RedisException;
-    
-    /**
-     * 根据uniqueKey构建分布式锁,如果锁可用   立即返回true，  否则返回false
-     * @param uniqueKey
-     * @return
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean tryLock(String uniqueKey) throws DistributedLockException,RedisException;
-    
-    /**
-     * 在给定时间内获取锁,如果获取锁则返回true,否则返回false
-     * @param uniqueKey
-     * @param timeout
-     * @param unit
-     * @return
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean tryLock(String uniqueKey, long timeout, TimeUnit unit) throws DistributedLockException,RedisException;
-    
-    /**
-     * 释放锁
-     * @param uniqueKey
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean unLock(String uniqueKey) throws DistributedLockException,RedisException;
     
     /**
      * 是否锁住的.
@@ -109,12 +89,22 @@ public interface DistributedLock {
      * 根据uniqueKey构建分布式锁,对key进行锁定,循环获取锁,直到获取到锁为止
      * @param uniqueKey:唯一的key,
      * @param uniqueValue:请求的value,可以使用UUID来获取.
+     * @return
+     * @throws DistributedLockException
+     * @throws RedisException
+     */
+    boolean getDistLock(int dbIndex,String uniqueKey,String uniqueValue) throws DistributedLockException,RedisException;
+    
+    /**
+     * 根据uniqueKey构建分布式锁,对key进行锁定,循环获取锁,直到获取到锁为止
+     * @param uniqueKey:唯一的key,
+     * @param uniqueValue:请求的value,可以使用UUID来获取.
      * @param expireTime:毫秒数.
      * @return
      * @throws DistributedLockException
      * @throws RedisException
      */
-    boolean getDistLock(int dbindex,String uniqueKey,String uniqueValue,int expireTime) throws DistributedLockException,RedisException;
+    boolean getDistLock(int dbIndex,String uniqueKey,String uniqueValue,int expireTime) throws DistributedLockException,RedisException;
     
     /**
      * 释放 uniqueKey 对应的锁资源.
@@ -124,43 +114,7 @@ public interface DistributedLock {
      * @throws DistributedLockException
      * @throws RedisException
      */
-    boolean releaseDistLock(int dbindex,String uniqueKey,String uniqueValue) throws DistributedLockException,RedisException;
-    
-    /**
-     * 根据uniqueKey构建分布式锁,对key进行锁定,循环获取锁,直到获取到锁为止
-     * @param uniqueKey
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean loopLock(int dbindex,String uniqueKey) throws DistributedLockException,RedisException;
-    
-    /**
-     * 根据uniqueKey构建分布式锁,如果锁可用   立即返回true，  否则返回false
-     * @param uniqueKey
-     * @return
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean tryLock(int dbindex,String uniqueKey) throws DistributedLockException,RedisException;
-    
-    /**
-     * 在给定时间内获取锁,如果获取锁则返回true,否则返回false
-     * @param uniqueKey
-     * @param timeout
-     * @param unit
-     * @return
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean tryLock(int dbindex,String uniqueKey, long timeout, TimeUnit unit) throws DistributedLockException,RedisException;
-    
-    /**
-     * 释放锁
-     * @param uniqueKey
-     * @throws DistributedLockException
-     * @throws RedisException
-     */
-    boolean unLock(int dbindex,String uniqueKey) throws DistributedLockException,RedisException;
+    boolean releaseDistLock(int dbIndex,String uniqueKey,String uniqueValue) throws DistributedLockException,RedisException;
     
     /**
      * 是否锁住的.
@@ -168,6 +122,6 @@ public interface DistributedLock {
      * @throws DistributedLockException
      * @throws RedisException
      */
-    boolean isLock(int dbindex,String uniqueKey) throws DistributedLockException,RedisException;
+    boolean isLock(int dbIndex,String uniqueKey) throws DistributedLockException,RedisException;
     
 }
