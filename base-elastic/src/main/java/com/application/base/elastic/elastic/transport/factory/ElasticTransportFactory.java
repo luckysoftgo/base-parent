e9ac53234aff4df2584ba67ca2ab6a1010774cf2
luckysoftgo <supingemail@126.com>
2019-12-04 18:28:12 +0800

    提交修改...

--- base-elastic/src/main/java/com/application/base/elastic/elastic/transport/factory/ElasticTransportFactory.java
+++ base-elastic/src/main/java/com/application/base/elastic/elastic/transport/factory/ElasticTransportFactory.java
@@ -82,7 +82,7 @@ public class ElasticTransportFactory implements PooledObjectFactory<TransportCli
 			// 集群名
 			.put("cluster.name", clusterName)
 			// 自动把集群下的机器添加到列表中:true.是;false.否
-			//.put("client.transport.sniff", isAppend)
+			.put("client.transport.sniff", true)
 			// 忽略集群名字验证, 打开后集群名字不对也能连接上
 			.put("client.transport.ignore_cluster_name", true)
 			.build();
