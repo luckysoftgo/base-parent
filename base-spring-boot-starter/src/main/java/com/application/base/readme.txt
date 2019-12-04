bcab26d1110325068afc75a85f7917e2117b715d
luckysoftgo <supingemail@126.com>
2019-12-04 14:35:09 +0800

    修改提交...

--- base-spring-boot-starter/src/main/java/com/application/base/readme.txt
+++
@@ -1,3 +0,0 @@
-
-此 start 为 base-* 系列 工程的 springboot 方式启动的配置类.
-
--- base-spring-boot-starter/src/main/java/com/application/base/starter/config/nacos/NocasConfig.java
+++
@@ -1,9 +0,0 @@
-package com.application.base.starter.config.nacos;
-
-/**
- * @author : 孤狼
- * @NAME: NocasConfig
- * @DESC:
- **/
-public class NocasConfig {
-}
