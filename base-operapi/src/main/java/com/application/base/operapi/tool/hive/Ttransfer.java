9987b88bca5c72f41e74390e0b243627526804cf
luckysoftgo <supingemail@126.com>
2019-12-25 14:27:13 +0800

    提交修改...

--- base-operapi/src/main/java/com/application/base/operapi/tool/hive/Ttransfer.java
+++ base-operapi/src/main/java/com/application/base/operapi/tool/hive/Ttransfer.java
@@ -9,6 +9,10 @@ import com.application.base.operapi.tool.hive.core.DataImportClient;
 /**
  * @author : 孤狼
  * @NAME: Ttransfer
+ *
+ * `    必须要有hadoop的安装路径.
+ *      核心思想是:将mysql中的数据抽出来,放入到一个文件中去,然后创建hive的表,把生成的数据文件上传到hdfs上去,然后挂载到hive上
+ *
  * @DESC: 提交转换.
  **/
 public class Ttransfer {
@@ -18,13 +22,14 @@ public class Ttransfer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.setProperty("","");
 		HiveConfig hiveConfig = new HiveConfig();
 		hiveConfig.setDriver("org.apache.hive.jdbc.HiveDriver");
-		hiveConfig.setUrl("jdbc:hive2://192.168.10.185:10000/default");
-		hiveConfig.setRemoteFilePath("/tmp/");
+		hiveConfig.setUrl("jdbc:hive2://192.168.10.185:10000/test");
+		hiveConfig.setRemoteFilePath("/user/hdfs/test/");
 		hiveConfig.setUsername("");
 		hiveConfig.setPassword("");
-		hiveConfig.setHdfsAddresss("hdfs://192.168.153.111:9000");
+		hiveConfig.setHdfsAddresss("hdfs://192.168.10.185:9000");
 		
 		JdbcConfig jdbcConfig = new JdbcConfig();
 		jdbcConfig.setDataBase("test");
@@ -35,7 +40,7 @@ public class Ttransfer {
 		jdbcConfig.setDriver("com.mysql.jdbc.Driver");
 		jdbcConfig.setHost("127.0.0.1");
 		jdbcConfig.setPort(3306);
-		jdbcConfig.setLocalTmpPath("E://upload/");
+		jdbcConfig.setLocalTmpPath("E://data/");
 		
 		SshConfig sshConfig = new SshConfig();
 		sshConfig.setHost("192.168.10.185");
