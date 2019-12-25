e8d275321cb6565b0be07cd5bd1031fd57f437e1
luckysoftgo <supingemail@126.com>
2019-12-25 11:03:30 +0800

    提交修改...

---
+++ base-operapi/src/main/java/com/application/base/operapi/api/hive/cont/Constants.java
@@ -0,0 +1,47 @@
+package com.application.base.operapi.api.hive.cont;
+
+/**
+ * @author : 孤狼
+ * @NAME: Constants
+ * @DESC:
+ * @DATE: 公共能常量
+ **/
+public class Constants {
+	
+	/**参数默认值*/
+	public static final String DEFAULT_PARAM_VAL = "";
+	
+	/**中文字符集*/
+	public static final String CHAR_UTF8 = "UTF-8";
+	
+	/**数据默认分隔符*/
+	public static final String DEFAULT_SEPAREATOR = "|";
+	
+	/**字段默认分隔符*/
+	public static final String DEFAULT_FIELDS_SEPAREATOR = ",";
+	
+	/**空格字符串*/
+	public static final String BLANK_SPACE = " ";
+	
+	/**换行字符*/
+	public static final String LINE_FEED = "\n";
+	
+	/**制表符字符*/
+	public static final String TAB = "\t";
+	/**
+	 * 空格.
+	 */
+	public static final String SPLIT_SPACE = " ";
+	/**
+	 * 转义符号
+	 */
+	public static final String TRANSFORM_QUOTES = "\"";
+	/**
+	 * jvm 设置
+	 */
+	public static final String JVM_CM = "--jvm=";
+	/**
+	 * 参数param
+	 */
+	public static final String PARAMS_CM = "-p ";
+}
